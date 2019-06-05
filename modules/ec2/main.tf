f10f6f5184e3902c91ba545168e22f9de427f52a
anthonyblutz@gmail.com <anthonyblutz@gmail.com>
2019-06-04 23:13:54 -0400

    automatic bootstrapping

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,37 @@ provider "aws" {
   profile = "${var.profile}"
   region = "${var.region}"
 }
+
+data "template_file" "init" {
+  template = <<-EOF
+  #!/bin/bash -xev
+
+  # Do some chef pre-work
+  /bin/mkdir -p /etc/chef
+  /bin/mkdir -p /var/lib/chef
+  /bin/mkdir -p /var/log/chef
+
+  cd /etc/chef/
+
+  # Copy the organization validator key
+  aws s3 cp s3://sts-demo-bucket/simpletechnologysolutions-validator.pem /etc/chef/simpletechnologysolutions-validator.pem
+
+  # Install chef
+  curl -L https://omnitruck.chef.io/install.sh | bash || error_exit 'could not install chef'
+
+  # Create client.rb
+  /bin/echo 'log_location     STDOUT' >> /etc/chef/client.rb
+  /bin/echo -e "chef_server_url  \"https://api.chef.io/organizations/simpletechnologysolutions\"" >> /etc/chef/client.rb
+  /bin/echo -e "validation_client_name \"simpletechnologysolutions-validator\"" >> /etc/chef/client.rb
+  /bin/echo -e "validation_key \"/etc/chef/simpletechnologysolutions-validator.pem\"" >> /etc/chef/client.rb
+  /bin/echo -e "chef_license \"accept\"" >> /etc/chef/client.rb
+
+  sudo chef-client
+  EOF
+}
+
 resource "aws_instance" "ec2" {
+  user_data = "${data.template_file.init.rendered}"
   ami           = "${var.ami}"
   instance_type = "${var.instance_type}"
 
